0e25152573fb091fe2f628b8eefdf52a27b41a8f
sreemaye <sreemaye44@gmail.com>
2023-11-15 12:34:23 +0600

    built in module

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-add(2,3);
+const { a, add } = require("./local-1"); //common js syntex
+const { a: a2, add: add2 } = require("./local-2");
+console.log(add(2, 3));
+console.log(add2(2, 3, 5));
+
+//build in module
+const path = require("path");
+console.log("🚀 ~ file: index.js:8 ~ path:", path.parse('/home/sreemaye/Next-Level/learning-node/index.js'))
+
